3294bc4723cd257e6604d8a3d26a355f75bcdcf0
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 18:34:54 +0300

    GameList with Database (MySQL)
    Give information about game such as category, publish date. In addition, user update data.

---
+++ week14-1/week14-1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace week14_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(344, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get list of Games";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(344, 484);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(437, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "id";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(464, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(37, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(464, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(317, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "title";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(464, 148);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(317, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(388, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(363, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "publish date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(464, 178);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(122, 26);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(414, 36);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 550);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ week14-1/week14-1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace week14_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConnection = new MySqlConnection("Server=localhost;Database=gamesdb;Uid=gamesdba;Pwd=gamesdba123;"))
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    string query = "SELECT id, title, category, publishDate from `game`";
+                    MySqlCommand sqlCommand = new MySqlCommand(query, sqlConnection);
+
+                    MySqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    //listBox1.DataSource = sqlDataReader; //not allowed
+
+                    List<KeyValuePair<int, string>> list = new List<KeyValuePair<int, string>>();
+                    while (sqlDataReader.Read())
+                    {
+                        list.Add(new KeyValuePair<int, string>(sqlDataReader.GetInt32(0), sqlDataReader["title"].ToString()));
+                    }
+                    sqlDataReader.Close();
+
+                    listBox1.DataSource = list;
+                    listBox1.DisplayMember = "Value";
+                    listBox1.ValueMember = "Key";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            var keyvaluepair = (KeyValuePair<int, string>)listBox1.SelectedItem;
+
+            using (MySqlConnection sqlConnection = new MySqlConnection("Server=localhost;Database=gamesdb;Uid=gamesdba;Pwd=gamesdba123;"))
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    string query = "SELECT id, title, category, publishDate from `game` WHERE id = @id";
+                    MySqlCommand sqlCommand = new MySqlCommand(query, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@id", keyvaluepair.Key);
+                    MySqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    //listBox1.DataSource = sqlDataReader; //not allowed
+
+                    if (sqlDataReader.Read())
+                    {
+                        textBox1.Text = sqlDataReader["id"].ToString();
+                        textBox2.Text = sqlDataReader["title"].ToString();
+                        textBox3.Text = sqlDataReader["category"].ToString();
+                        dateTimePicker1.Value = DateTime.Parse(sqlDataReader["publishDate"].ToString());
+                    }
+                    sqlDataReader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+                return;
+
+            using (MySqlConnection sqlConnection = new MySqlConnection("Server=localhost;Database=gamesdb;Uid=gamesdba;Pwd=gamesdba123;"))
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    string query = "UPDATE `game` set title=@title, category=@category, publishDate=@publishDate WHERE id = @id";
+                    MySqlCommand sqlCommand = new MySqlCommand(query, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@id", Convert.ToInt32(textBox1.Text));
+                    sqlCommand.Parameters.AddWithValue("@title", textBox2.Text);
+                    sqlCommand.Parameters.AddWithValue("@category", textBox3.Text);
+                    sqlCommand.Parameters.AddWithValue("@publishDate", dateTimePicker1.Value);
+
+                    var affectedRowCount = sqlCommand.ExecuteNonQuery();
+                    if (affectedRowCount > 0)
+                    {
+                        MessageBox.Show($"{affectedRowCount} rows updated. ");
+                        button1_Click(sender, e);
+                    }                        
+                    else
+                        MessageBox.Show("No rows updated. ");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+    }
+}
